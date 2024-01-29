c080d8d0a7f9ae0aaf1ab04d423ee864613b65e2
h3000plus <hridoy.3000plus@gmail.com>
2024-01-29 23:12:24 +0600

    order integrated

--- dist/middlewares/protect.middleware.js
+++ dist/middlewares/protect.middleware.js
@@ -1,20 +1,22 @@
-import { verifyToken } from '../utilities/auth.js';
+import { verifyToken } from "../utilities/auth.js";
 const protectMiddleware = (req, res, next) => {
     const token = req.headers.token;
     if (!token) {
         res.status(401);
-        res.json({ message: 'You are not authorized' });
+        res.json({ message: "You are not authorized" });
         return;
     }
     try {
         const user = verifyToken(token);
+        if (!user)
+            throw Error("user doesnt exist");
         req.body.user = user;
         next();
     }
     catch (error) {
         console.log(error);
         res.status(401);
-        res.json({ message: 'You are not authorized' });
+        res.json({ message: error });
         return;
     }
 };
