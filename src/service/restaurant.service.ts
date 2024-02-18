d43c8d69fff5be4b8680d66de1e5b19dff4de3da
noel.alam <noel.alam9999@gmail.com>
2024-02-18 13:40:03 +0600

    feat:restaurant service

--- src/service/restaurant.service.ts
+++ src/service/restaurant.service.ts
@@ -27,7 +27,7 @@ export async function getFilteredRestaurants(
       return {
         resId: res.restaurantId,
         resName: res.restaurantName,
-        resImage: res.restaurantLogo,
+        resImage: res.restaurantCoverPhoto,
       };
     });
 
